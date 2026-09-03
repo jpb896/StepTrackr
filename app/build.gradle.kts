1a3d7a9e8dfd0d0bd94878deab02f38b7d35bc74
jpb <jpb44173@gmail.com>
2026-09-03 23:46:39 +0100

    feat/step: Add GMS Health and Jetpack WorkManager
    One is for background step tracking on GMS, while the other is to be able to allow collection of step data in the background for non-GMS devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.play.services.fitness)
+    implementation(libs.androidx.work.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
