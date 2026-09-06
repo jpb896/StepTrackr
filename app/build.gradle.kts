f8dbed6c1a398c7297826586959749a941403809
jpb <jpb44173@gmail.com>
2026-09-06 23:44:24 +0100

    feat/step: Add non-GMS step service
    This uses the SensorManager API as a base and displays a persistent notification, letting the user know that steps are currently being counted.
    This implementation will not be used if GMS is present on the device.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jpb.steptrackr"
-        minSdk = 27
+        minSdk = 34
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
---
+++ app/src/main/java/com/jpb/steptrackr/services/NonGmsStepService.kt
@@ -0,0 +1,55 @@
+package com.jpb.steptrackr.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.core.app.NotificationCompat
+import com.jpb.steptrackr.R
+import kotlin.time.Instant
+
+class NonGmsStepService : Service(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var lastSavedSteps = 0L
+
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        // Android requires a persistent notification for foreground services
+        val notification = NotificationCompat.Builder(this, "step_channel")
+            .setContentTitle("Tracking Steps")
+            .setSmallIcon(R.drawable.ic_walk)
+            .build()
+
+        startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH)
+
+        stepSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+            val totalStepsSinceBoot = event.values[0].toLong()
+
+            if (lastSavedSteps > 0 && totalStepsSinceBoot > lastSavedSteps) {
+                val delta = totalStepsSinceBoot - lastSavedSteps
+                saveStepsToLocalDatabase(delta, Instant.now())
+            }
+            lastSavedSteps = totalStepsSinceBoot
+        }
+    }
+
+    private fun saveStepsToLocalDatabase(delta: Long, timestamp: Instant) {
+        //TODO:  Log this locally. Your WorkManager sync block will pick these entries up later.
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    override fun onBind(intent: Intent?) = null
+}
